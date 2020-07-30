9337d4afade04cee1027e7de832655b354ce5ce7
Manuel Ganter <manuel.ganter@hotmail.de>
2020-07-30 11:27:21 +0200

    not initial commit

--- sum/sum.go
+++ sum/sum.go
@@ -6,3 +6,29 @@ func Sum(numbers []int) (sum int) {
 	}
 	return sum
 }
+
+// SumAll sums slices in slices
+func SumAll(summandSlices ...[]int) []int {
+	results := make([]int, len(summandSlices))
+
+	for index, summandSlice := range summandSlices {
+		results[index] = Sum(summandSlice)
+	}
+
+	return results
+}
+
+// SumAllTails sums the tails of each slice
+func SumAllTails(summandSlices ...[]int) []int {
+	results := make([]int, len(summandSlices))
+
+	for index, summandSlice := range summandSlices {
+		if len(summandSlice) == 0 {
+			continue
+		}
+
+		results[index] = Sum(summandSlice[1:])
+	}
+
+	return results
+}
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -15,15 +18,32 @@ func TestSum(t *testing.T) {
 		}
 	})
 
-	//t.Run("collection of any size", func(t *testing.T) {
-	//	numbers := []int{1, 2, 3}
-	//
-	//	got := Sum(numbers)
-	//	want := 6
-	//
-	//	if got != want {
-	//		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	//	}
-	//})
+}
 
+func TestSumAll(t *testing.T) {
+	t.Run("collection of collection of numbers", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("need better names", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9, 4})
+		want := []int{0, 13}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2}, []int{0, 9})
+	}
 }
